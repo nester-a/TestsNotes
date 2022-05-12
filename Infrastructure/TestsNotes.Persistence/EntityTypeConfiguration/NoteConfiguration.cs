e004124c5a728a4bf7bf53a5b0abb0ffd1651d42
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-12 21:20:38 +0300

    класс для конфигурации БД

---
+++ Infrastructure/TestsNotes.Persistence/EntityTypeConfiguration/NoteConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Persistence.EntityTypeConfiguration;
+
+public class NoteConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.HasIndex(x => x.Id).IsUnique();
+        builder.Property(x => x.Title).HasMaxLength(250);
+    }
+}
