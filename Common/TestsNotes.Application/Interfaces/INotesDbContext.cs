0a4a6e0df4b4d82d17366dd19c7b2edbefcb40b5
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-12 21:26:49 +0300

    интерфейс БД контекста

--- Common/TestsNotes.Application/Interfaces/INotesDbContext.cs
+++ Common/TestsNotes.Application/Interfaces/INotesDbContext.cs
@@ -1,6 +1,10 @@
-﻿namespace TestsNotes.Application.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Interfaces;
 
 public interface INotesDbContext
 {
-
+    DbSet<Note> Notes { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
