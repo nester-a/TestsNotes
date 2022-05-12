551fd5b43c2b4b643448882fe5626fe0b93acac8
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-12 22:14:35 +0300

    обработчик команды создания заметки

---
+++ Common/TestsNotes.Application/Notes/Commands/CreateNote/CreateNoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using TestsNotes.Application.Interfaces;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Notes.Commands.CreateNote
+{
+    public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Guid>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public CreateNoteCommandHandler(INotesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Guid> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+        {
+            var note = new Note()
+            {
+                UserId = request.UserId,
+                Title = request.Title,
+                Details = request.Details,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null,
+            };
+            
+            await _dbContext.Notes.AddAsync(note, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return note.Id;
+        }
+    }
+}
