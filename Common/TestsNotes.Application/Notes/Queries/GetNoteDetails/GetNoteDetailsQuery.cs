cb5717b78d1b2cc24b656893894ace09eeb863df
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-13 21:04:21 +0300

    вьюмодель деталей заметки

---
+++ Common/TestsNotes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TestsNotes.Application.Notes.Queries.GetNoteDetails;
+public class GetNoteDetailsQuery : IRequest<NoteDetailsVm>
+{
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+}
---
+++ Common/TestsNotes.Application/Notes/Queries/GetNoteDetails/NoteDetailsVm.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using TestsNotes.Application.Common.Mappings;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Notes.Queries.GetNoteDetails;
+public class NoteDetailsVm : IMapWith<Note>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Details { get; set; }
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Note, NoteDetailsVm>()
+            .ForMember(vm => vm.Title,
+                opt => opt.MapFrom(n => n.Title))
+            .ForMember(vm => vm.Details,
+                opt => opt.MapFrom(n => n.Details))
+            .ForMember(vm => vm.CreationDate,
+                opt => opt.MapFrom(n => n.CreationDate))
+            .ForMember(vm => vm.EditDate,
+                opt => opt.MapFrom(n => n.EditDate));
+    }
+}
