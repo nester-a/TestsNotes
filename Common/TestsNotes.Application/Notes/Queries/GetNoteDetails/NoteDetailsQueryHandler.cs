fadfba407e9196f461cd74f3698baab054f50717
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-13 21:12:42 +0300

    обработчик получения деталей заметки

---
+++ Common/TestsNotes.Application/Notes/Queries/GetNoteDetails/NoteDetailsQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TestsNotes.Application.Common.Exceptions;
+using TestsNotes.Application.Interfaces;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Notes.Queries.GetNoteDetails
+{
+    public class NoteDetailsQueryHandler : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public NoteDetailsQueryHandler(INotesDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes.FirstOrDefaultAsync(n => n.Id == request.Id, cancellationToken);
+
+            if (entity is null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
