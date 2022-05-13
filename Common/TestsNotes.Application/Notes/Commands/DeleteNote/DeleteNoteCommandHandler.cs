33904bf7199c93b45f89c547a0d685426a4d5e76
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-13 20:53:02 +0300

    обработчик команды удаления

---
+++ Common/TestsNotes.Application/Notes/Commands/DeleteNote/DeleteNoteCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using TestsNotes.Application.Common.Exceptions;
+using TestsNotes.Application.Interfaces;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Notes.Commands.DeleteNote;
+public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand>
+{
+    private readonly INotesDbContext _dbContext;
+
+    public DeleteNoteCommandHandler(INotesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _dbContext.Notes.FindAsync(new object[] { request.Id }, cancellationToken);
+        if (entity is null || entity.UserId != request.UserId)
+        {
+            throw new NotFoundException(nameof(Note), request.Id);
+        }
+
+        _dbContext.Notes.Remove(entity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
