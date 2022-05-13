7838754871cbd2c83f8afccc88982039bf20af4c
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-13 21:18:41 +0300

    дто для просмотра заметки

---
+++ Common/TestsNotes.Application/Notes/Queries/GetNotesList/NoteLookupDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TestsNotes.Application.Common.Mappings;
+using TestsNotes.Domain;
+
+namespace TestsNotes.Application.Notes.Queries.GetNotesList;
+public class NoteLookupDto : IMapWith<Note>
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Note, NoteLookupDto>()
+            .ForMember(dto => dto.Id,
+                opt => opt.MapFrom(n => n.Id))
+            .ForMember(dto => dto.Title,
+                opt => opt.MapFrom(n => n.Title));
+    }
+}
