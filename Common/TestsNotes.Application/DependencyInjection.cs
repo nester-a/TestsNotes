53547330d40f55741385dc232f8693f23ead4943
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-16 22:28:10 +0300

    добавление методов расширений для внедрения зависимостей

---
+++ Common/TestsNotes.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace TestsNotes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection collection)
+        {
+            collection.AddMediatR(Assembly.GetExecutingAssembly());
+            return collection;
+        }
+    }
+}
